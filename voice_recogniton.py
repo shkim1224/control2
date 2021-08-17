a341adb22a090950bc4ee1ea1712db0faf4c8119
shkim1224 <shkim@kunsan.ac.kr>
2021-08-17 11:52:52 +0900

    Delete voice_recogniton.py

--- voice_recogniton.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-print(sr.__version__)
-r = sr.Recognizer()
-
-harvard = sr.AudioFile('harvard.wav')
-with harvard as source:
-    audio = r.record(source)
-
-print(r.recognize_google(audio))
