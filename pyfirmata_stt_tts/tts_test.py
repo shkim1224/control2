ed95a30535d9cdd4fdd9c9745c1338e6fd330e37
KimSungho <shkim@kunsan.ac.kr>
2021-08-17 14:04:19 +0900

    stt-tts 변경

--- pyfirmata_stt_tts/tts_test.py
+++ pyfirmata_stt_tts/tts_test.py
@@ -1,3 +1,7 @@
+# 본 프로그램이 수행되기 위해서는 control 가상환경에 gtts가 설치되어야 함
+# conda에서 control을 활성화한 후
+# (control) > pip install gtts playsound 
+
 import speech_recognition as sr 
 from gtts import gTTS 
 import os 
--- pyfirmata_stt_tts/voice_recog_with_wavefile.py
+++ pyfirmata_stt_tts/voice_recog_with_wavefile.py
@@ -2,7 +2,10 @@ import speech_recognition as sr
 print(sr.__version__)
 r = sr.Recognizer()
 
-harvard = sr.AudioFile("F:\2021 2학기 강좌\control2\control2\pyfirmata_stt_tts\harvard.wav")
+#harvard = sr.AudioFile("F:\\2021_2_lecture\\control2\\pyfirmata_stt_tts\\harvard.wav")
+harvard = sr.AudioFile("F:/2021_2_lecture/control2/pyfirmata_stt_tts/harvard.wav")
+# sr.AudioFile()에 들어가는 파일 path는 전체 파일 패스를 넣어야 함
+# 또한 윈도우의 경우 \\를 사용하던지 아니면 /를 사용해야 함
 with harvard as source:
     audio = r.record(source)
     out_text = r.recognize_google(audio)
