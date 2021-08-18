35e438b184d05dd9894830d3bd05e83bec515e62
KimSungho <shkim@kunsan.ac.kr>
2021-08-18 13:18:17 +0900

    정보제어설계입문-3주차-2

---
+++ python_basic/python_class_basic/class_test.py
@@ -0,0 +1,16 @@
+class Rectangle:
+    count = 0  # 클래스 변수
+ 
+    # 초기자(initializer)
+    def __init__(self, width, height):
+        # self.* : 인스턴스변수
+        self.width = width
+        self.height = height
+        Rectangle.count += 1
+ 
+    # 메서드
+    def calcArea(self):
+        area = self.width * self.height
+        return area
+    
+
